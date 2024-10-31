5fcdcc0114b2daaedbb50b0139e633dc19113d13
spencscott <73132546+spencscott@users.noreply.github.com>
2024-10-31 11:54:20 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('softphone-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './jssip-3.10.0.min.js',
+        './icon-192x192.png',
+        './icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
