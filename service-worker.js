e430178d6c0df9f6e6f0f12cf4295516c21148f4
spencscott <73132546+spencscott@users.noreply.github.com>
2024-11-22 13:39:34 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,6 @@ self.addEventListener('install', function(event) {
     })
   );
 });
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
